4d409c837aac323283f8aa563e71ca6934c6b3cb
Giao Quỳnh <giaovan99@gmail.com>
2024-05-18 13:13:32 +0700

    update cụm trang resources

--- js/sourcing.js
+++ js/sourcing.js
@@ -1,22 +1,21 @@
-let navLinks = document.querySelectorAll(".section-table-crop .nav-link");
-console.log(navLinks);
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", () => {
-    let type = navLink.getAttribute("crop-type");
-    let img = document.querySelector(".section-table-crop .tab-pane img");
-    switch (type) {
-      case "cassia":
-        img.src = "./assets/img/sourcing-page/crop-1.jpeg";
-        break;
-      case "pepper":
-        img.src = "./assets/img/sourcing-page/crop-2.jpeg";
-        break;
-      case "anise":
-        img.src = "./assets/img/sourcing-page/crop-3.jpeg";
-        break;
-      default:
-        img.src = "./assets/img/sourcing-page/crop-1.jpeg";
-        break;
-    }
-  });
-});
+// let navLinks = document.querySelectorAll(".section-table-crop .nav-link");
+// navLinks.forEach((navLink) => {
+//   navLink.addEventListener("click", () => {
+//     let type = navLink.getAttribute("crop-type");
+//     let img = document.querySelector(".section-table-crop .tab-pane img");
+//     switch (type) {
+//       case "cassia":
+//         img.src = "./assets/img/sourcing-page/crop-1.jpeg";
+//         break;
+//       case "pepper":
+//         img.src = "./assets/img/sourcing-page/crop-2.jpeg";
+//         break;
+//       case "anise":
+//         img.src = "./assets/img/sourcing-page/crop-3.jpeg";
+//         break;
+//       default:
+//         img.src = "./assets/img/sourcing-page/crop-1.jpeg";
+//         break;
+//     }
+//   });
+// });
